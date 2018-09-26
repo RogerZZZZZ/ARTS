d3c347a170053a82f8e37cd0191c8152553d0ac6
RogerZZZZZ <501841352@qq.com>
2018-09-26 22:28:28 +0800

    add algorithm two

--- Week_2018-9-9/algorithm two.js
+++ Week_2018-9-9/algorithm two.js
@@ -1,7 +1,17 @@
 /**
+ * @version 1 100%
  * @param {number[]} A
  * @return {number}
  */
-var peakIndexInMountainArray = function(A) {
-    
+var flipAndInvertImage = function(A) {
+    return A.map(item => {
+      item = item.reverse()
+      return item.map(it => {
+        return 1 - it
+      })
+    })
 };
+
+const data = [[1,1,0,0],[1,0,0,1],[0,1,1,1],[1,0,1,0]]
+
+console.log(flipAndInvertImage(data))
