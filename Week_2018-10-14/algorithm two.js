1f4f11a243ceec54704566de02d4b82e8e3a3421
RogerZZZZZ <501841352@qq.com>
2018-11-06 23:55:47 +0800

    optimize the algorithm answer

--- Week_2018-10-14/algorithm two.js
+++ Week_2018-10-14/algorithm two.js
@@ -1,5 +1,30 @@
+// /**
+//  * @version 1 5.94%
+//  * Definition for a binary tree node.
+//  * function TreeNode(val) {
+//  *     this.val = val;
+//  *     this.left = this.right = null;
+//  * }
+//  */
+// /**
+//  * @param {TreeNode} root
+//  * @param {number} val
+//  * @return {TreeNode}
+//  */
+// var searchBST = function(root, val) {
+//     if (root === null) return []
+//     const rootVal = root.val
+//     if (rootVal === val) return root
+//     if (rootVal < val) {
+//       return searchBST(root.right, val)
+//     } else {
+//       return searchBST(root.left, val)
+//     }
+// };
+
+
 /**
- * @version 1 5.94%
+ * @version 2 98.16%
  * Definition for a binary tree node.
  * function TreeNode(val) {
  *     this.val = val;
@@ -12,12 +37,13 @@
  * @return {TreeNode}
  */
 var searchBST = function(root, val) {
-    if (root === null) return []
-    const rootVal = root.val
-    if (rootVal === val) return root
-    if (rootVal < val) {
-      return searchBST(root.right, val)
-    } else {
-      return searchBST(root.left, val)
-    }
+  if (root === null) return []
+  const rootVal = root.val
+  if (rootVal === val) {
+    return root
+  } else if (rootVal < val) {
+    return searchBST(root.right, val)
+  } else {
+    return searchBST(root.left, val)
+  }
 };
