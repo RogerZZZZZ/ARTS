b54239f3bc3eb4b767f2a9712989895cce0a83ab
RogerZZZZZ <501841352@qq.com>
2018-11-06 23:52:12 +0800

    add algorithm

--- Week_2018-10-14/README.md
+++ Week_2018-10-14/README.md
@@ -2,6 +2,10 @@
 
 ## Algorithm
 
+[933. Number of Recent Calls](https://leetcode.com/problems/number-of-recent-calls/description/)
+
+[700. Search in a Binary Search Tree](https://leetcode.com/problems/search-in-a-binary-search-tree/description/)
+
 ## Review
 
 ## Tips
---
+++ Week_2018-10-14/algorithm two.js
@@ -0,0 +1,23 @@
+/**
+ * @version 1 5.94%
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var searchBST = function(root, val) {
+    if (root === null) return []
+    const rootVal = root.val
+    if (rootVal === val) return root
+    if (rootVal < val) {
+      return searchBST(root.right, val)
+    } else {
+      return searchBST(root.left, val)
+    }
+};
