b33ed75cc14d329e398910060daa5c22bbd3092e
RogerZZZZZ <501841352@qq.com>
2019-01-06 23:37:43 +0800

    add algorithm two

--- Week_2018-11-25/algorithm two.js
+++ Week_2018-11-25/algorithm two.js
@@ -1,20 +1,31 @@
-/**
- * @param {string[][]} accounts
- * @return {string[][]}
- */
-var accountsMerge = function(accounts) {
-  const group = groupByName(accounts, (el) => el[0])
-  console.log(group)
+function accountsMerge(accounts) {
+  const roots = new Set();
+  const owner = {};
+  const parent = {};
+  const children = {};
 
-};
+  for (let account of accounts) {
+    let [user, root, ...emails] = account;
+    let r1 = find(root);
+    owner[root] = user;
+    children[r1] = children[r1] || [root];
+    roots.add(r1);
 
-const groupByName = (arr, fn) =>
-  arr.map(typeof fn === 'function' ? fn : val => val[fn]).reduce((acc, val, i) => {
-    acc[val] = (acc[val] || [])
-    acc[val].push(arr[i])
-    return acc;
-  }, {});
+    for (let email of emails) {
+      let r2 = find(email);
+      if (r2 !== r1) {
+        parent[r2] = r1;
+        children[r1].push(...(children[r2] ? children[r2] : [email]));
+        roots.delete(r2);
+        delete children[r2];
+      }
+    }
+  }
 
+  return [...roots].map(r => [owner[r], ...children[r].sort()]);
 
-
-console.log(accountsMerge([["John", "johnsmith@mail.com", "john00@mail.com"], ["John", "johnnybravo@mail.com"], ["John", "johnsmith@mail.com", "john_newyork@mail.com"], ["Mary", "mary@mail.com"]]))
+  function find(a) {
+    parent[a] = parent[a] || a;
+    return a === parent[a] ? a : find(parent[a]);
+  }
+}
